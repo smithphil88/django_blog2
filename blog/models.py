c08693c356a87b218e426763bc9cd5973dd4027e
Phil Smith <smithphil868@gmail.com>
2024-11-10 11:02:03 +0000

    add comments f string to comment section of blog

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,11 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"The title of this post is {self.title}"
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -23,3 +28,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
