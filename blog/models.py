d154c9a6c5f0dfd9357deea0b2531c0153ef2c3e
Phil Smith <smithphil868@gmail.com>
2024-11-11 11:27:36 +0000

    add view page to the blog

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+
     class Meta:
         ordering = ["-created_on"]
 
@@ -28,6 +29,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
 
     class Meta:
         ordering = ["-created_on"]
